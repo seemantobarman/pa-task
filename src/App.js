fb420ca5446079dad77d6d21d051c3e6fa98e1de
Seemanto Barman <seemanto@hotmail.com>
2022-03-17 21:25:48 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Navbar />
         </div>
     );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>
+                <img
+                    src="https://assets.prothomalo.com/prothomalo/assets/palo-bangla-bb996cdb70d2e0ccec8c.svg"
+                    alt="Prothom Alo Logo"
+                    className="logo"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
